dce6476edef35794d11976af507e8b6eb33a4545
David Fox <dsf@seereason.com>
2020-02-17 14:34:05 -0800

    * Move some of the ErrorControl instances into Extra.ErrorControlInstances * Add delete and insert methods to the Member class in Extra.ErrorSet * Add a OneOf newtype wrappr and a Member instance for it

--- Extra/ErrorControl.hs
+++ Extra/ErrorControl.hs
@@ -38,37 +38,6 @@ instance ErrorControl e (Either e) Identity where
   controlError ma f = either f Identity ma
   accept = Right . runIdentity
 
-instance ErrorControl IOException IO UIO where
-  controlError ma f = unsafeFromIO (ma `catchError` (accept . f))
-  accept = run
-
-instance Monad m => ErrorControl e (ExceptT e m) m where
-  controlError ma f = runExceptT ma >>= either f pure
-  accept = lift
-
--- | Resolve the error on the left side of an Either.
-instance Monad m => ErrorControl (Either e1 e2) (ExceptT (Either e1 e2) m) (ExceptT e2 m) where
-  controlError :: ExceptT (Either e1 e2) m a -> (Either e1 e2 -> ExceptT e2 m a) -> ExceptT e2 m a
-  controlError ma f =
-    ExceptT (pivot <$> runExceptT ma) >>= either pure (f . Left)
-    where
-      pivot :: Either (Either a b) c -> Either b (Either c a)
-      pivot = either (either (Right . Right) Left) (Right . Left)
-  accept :: ExceptT e2 m a -> ExceptT (Either e1 e2) m a
-  accept = withExceptT Right
-
-#if 0
--- | Resolve the error on the right side of an Either.  (It turns out
--- this actually does conflict with the one above.)
-instance Monad m => ErrorControl (Either e1 e2) (ExceptT (Either e1 e2) m) (ExceptT e1 m) where
-  controlError ma f =
-    ExceptT (pivot <$> runExceptT ma) >>= either (f . Right) pure
-    where
-      pivot :: Either (Either a b) c -> Either a (Either b c)
-      pivot = either (either Left (Right . Left)) (Right . Right)
-  accept = withExceptT Left
-#endif
-
 instance ErrorControl e m n => ErrorControl e (StateT s m) (StateT s n) where
   controlError sma f = StateT (\s -> controlError (runStateT sma s) (\e -> runStateT (f e) s))
   accept = mapStateT accept
--- Extra/ErrorSet.hs
+++ Extra/ErrorSet.hs
@@ -1,19 +1,20 @@
-{-# LANGUAGE ConstraintKinds, DataKinds, DeriveAnyClass, DeriveGeneric, FlexibleContexts, FlexibleInstances, MultiParamTypeClasses, ScopedTypeVariables, TypeApplications, TypeFamilies #-}
+{-# LANGUAGE ConstraintKinds, DataKinds, DeriveAnyClass, DeriveGeneric, FlexibleContexts, FlexibleInstances, InstanceSigs, MultiParamTypeClasses, ScopedTypeVariables, TypeApplications, TypeFamilies #-}
 
 -- | Type level "sets" as a cons list constructed of Eithers.
 
 module Extra.ErrorSet
   ( Insert
   , Delete
-  , Member(follow)
-  -- * Re-export
-  -- , Void
+  , Member(follow, delete, insert)
+  , OneOf(OneOf)
   ) where
 
 import Control.Lens
 import Data.SafeCopy
--- import Data.Void (Void)
+import Data.Proxy (Proxy(Proxy))
+import Generic.Data.Internal.Newtype (Newtype, Old, pack, unpack)
 import GHC.Generics
+import Unsafe.Coerce
 
 -- See SafeCopy issue #78
 -- instance SafeCopy Void
@@ -22,13 +23,55 @@ type family Insert t s where
   Insert t (Either t a) = Either t a
   Insert t () = Either t ()
   Insert t (Either a b) = Either a (Insert t b)
+  Insert t (OneOf es) = OneOf (Insert t es)
 
 type family Delete t s where
   Delete t (Either t a) = a
   Delete t () = ()
   Delete t (Either a b) = Either a (Delete t b)
+  Delete t (OneOf es) = OneOf (Delete t es)
 
-class Member t set where follow :: Prism' set t
-instance Member t (Either t a) where follow = _Left
-instance {-# OVERLAPS #-} Member t b => Member t (Either a b) where follow = _Right . follow
-instance Member t () where follow = error "Type Set Error"
+class Member t set where
+  follow :: Prism' set t
+  delete :: Proxy t -> set -> Either t (Delete t set)
+  insert :: Proxy t -> Delete t set -> set
+
+instance Member t (Either t a) where
+  follow = _Left
+  delete :: Proxy t -> (Either t a) -> Either t (Delete t (Either t a)) -- Maybe a
+  delete proxy (Left t) = Left t
+  delete proxy (Right a) = Right a -- We know a doesn't contain t, so return it
+  insert :: a ~ Delete t (Either t a) => Proxy t -> a -> Either t a
+  insert proxy = Right -- there can't be a t in Delete t a
+
+instance {-# OVERLAPS #-} Member t b => Member t (Either a b) where
+  follow = _Right . follow
+  delete proxy (Left a) =
+    -- We know t is not a so this Left will remain a Left
+    Right (unsafeCoerce (Left a) :: Delete t (Either a b))
+  delete proxy (Right b) =
+    -- We know t is somewhere in b
+    case delete proxy b of
+      -- we found t and deleted it
+      Left t -> Left t
+      -- We found something other than t.  The compiler doesn't know
+      -- that somewhere in b there is a t, so we have to tell it what
+      -- Delete t (Either a b) is.
+      Right r -> Right (unsafeCoerce (Right r) :: Delete t (Either a b))
+  insert :: Proxy t -> Delete t (Either a b) -> Either a b
+  -- Tell the compiler that Delete t (Either a b) ~ Either a (Delete t b) here
+  insert proxy d = case (unsafeCoerce d :: Either a (Delete t b)) of
+                     Left a -> Left a
+                     Right d' -> Right (insert proxy d')
+
+instance Member t () where
+  follow = error "Type Set Error"
+  delete proxy = error "Type Set Error"
+  insert proxy = error "Type Set Error"
+
+newtype OneOf es = OneOf es deriving (Eq, Ord, Show, Functor)
+
+instance Member e es => Member e (OneOf es) where
+  follow = iso (\(OneOf es) -> es) OneOf . follow
+  delete proxy (OneOf es) = fmap OneOf (delete proxy es)
+  insert proxy (OneOf es) = OneOf (insert proxy es)
